c3ffa1b7ed92765c9a8ccda5961cbae3a93b656a
Brian Beach <coder@beachfamily.net>
2011-04-01 07:47:10 -0700

    Fix copyright.

--- tinymock/__init__.py
+++ tinymock/__init__.py
@@ -2,7 +2,7 @@
 # 
 # File: __init__.py
 # 
-# Copyright 2011 TiVo Inc. All Rights Reserved. by Brian Beach and Jaran Charumilind
+# Copyright 2011 by Brian Beach and Jaran Charumilind
 # 
 # This software is licensed under the MIT license.
 # 
